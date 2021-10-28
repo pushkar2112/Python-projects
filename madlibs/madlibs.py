1cd1922fe079f889bbd2094306f3aa05bbb9ff1c
Pushkar <Pushkar7397@gmail.com>
2021-10-28 17:49:13 +0530

    while condition checked

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -2,7 +2,6 @@ from sample_madlibs import coding, hungergames, potter, zombie
 # import sample_madlibs
 import random
 
-ctr = 0
 ch = None
 story = ['coding','hungergames', 'potter','zombie']
 LibModes = [coding, hungergames, potter, zombie]
@@ -10,7 +9,7 @@ LibModes = [coding, hungergames, potter, zombie]
 if __name__ == "__main__":
     print("Welcome to MAD Lib")
     
-    while ctr == 0:
+    while True:
         print("Do you want to play in Random Mode or Custom Mode? (R/C) ")
         ch = input().lower()
         if ch in ["r",'c']:
@@ -25,7 +24,6 @@ if __name__ == "__main__":
                     index = story.index(mode)
                     lib = LibModes[index]
                     lib.madlib()
-                    ctr +=1
                     break
                 else:
                     print('That  is not a valid input! Please try again...')
